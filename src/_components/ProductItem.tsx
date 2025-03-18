6df107241ccdd67bcd85b4da426a5c89ee97404b
Jintae Song <w.jinttao@gmail.com>
2025-03-19 03:59:32 +0900

    Design: change ProductItem layout, style

--- src/_components/ProductItem.tsx
+++ src/_components/ProductItem.tsx
@@ -2,21 +2,20 @@ import Image from "next/image";
 
 import { Product } from "@/types";
 
+const IMAGE_SIZE = 500;
+
 export default function ProductItem({ item }: { item: Product }) {
   return (
-    <div
-      key={item.id}
-      className="flex flex-col gap-2 items-center justify-center border-2 border-gray-300 rounded-md p-4"
-    >
+    <div key={item.id} className="flex flex-col gap-2 items-center justify-center p-4">
       <Image
         src={item.images[0]}
         alt={item.title}
-        width={100}
-        height={100}
-        className="w-[50%] h-auto"
+        width={IMAGE_SIZE}
+        height={IMAGE_SIZE}
+        className="w-full h-full max-w-xs max-h-xs rounded"
         priority
       />
-      <span className="text-lg font-bold">{item.title}</span>
+      <span className="text-lg font-semibold">{item.title}</span>
       <span className="text-gray-500 line-through">${item.price}</span>
     </div>
   );
