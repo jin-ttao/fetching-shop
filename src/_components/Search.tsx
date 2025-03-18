a5c33e2e5a8009a50016e1e2888b0d8cfcf538d6
Jintae Song <w.jinttao@gmail.com>
2025-03-19 06:32:09 +0900

    Feat: add search auto complete

--- src/_components/Search.tsx
+++ src/_components/Search.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import { Product } from "@/types";
 
 interface SearchProps {
   searchQuery: string;
@@ -9,14 +11,49 @@ interface SearchProps {
 
 export default function Search({ searchQuery, setSearchQuery }: SearchProps) {
   const [inputValue, setInputValue] = useState<string>(searchQuery);
+  const [options, setOptions] = useState<{ id: number; title: string }[]>([]);
+  const [isOpenOptions, setIsOpenOptions] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (inputValue.length > 0) {
+      const fetchOptions = async () => {
+        try {
+          const response = await fetch(
+            `https://api.escuelajs.co/api/v1/products?title=${inputValue}`
+          );
+          const data = await response.json();
+          setOptions(
+            data.map((item: Product) => {
+              return {
+                id: item.id,
+                title: item.title,
+              };
+            })
+          );
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      fetchOptions();
+    }
+  }, [inputValue]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
+    setIsOpenOptions(true);
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setSearchQuery(inputValue);
+    setIsOpenOptions(false);
+  };
+
+  const handleOptionClick = (option: { id: number; title: string }) => {
+    setInputValue(option.title);
+    setSearchQuery(option.title);
+    setOptions([]);
+    setIsOpenOptions(false);
   };
 
   return (
@@ -25,10 +62,23 @@ export default function Search({ searchQuery, setSearchQuery }: SearchProps) {
         <input
           type="text"
           value={inputValue}
-          className="w-[20vw] h-10 border-2 border-gray-300 rounded px-2"
+          className="relative w-[20vw] h-10 border-2 border-gray-300 rounded px-2"
           onChange={handleInputChange}
         />
       </form>
+      {inputValue && isOpenOptions && options.length > 0 && (
+        <div className="absolute top-20 left-10 w-[20vw] h-[30vh] overflow-y-auto bg-white border-2 border-gray-300 rounded">
+          {options.map((option) => (
+            <div
+              key={option.id}
+              className="p-2 hover:bg-gray-100 cursor-pointer"
+              onClick={() => handleOptionClick(option)}
+            >
+              {option.title}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
