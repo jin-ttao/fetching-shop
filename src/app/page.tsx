163b7eab581ac0858025ba0103a13ea85421c9fd
Jintae Song <w.jinttao@gmail.com>
2025-03-18 20:23:30 +0900

    Feat: add fetch product list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,25 @@
 import Image from "next/image";
 
-export default function Home() {
-  const mockItemList = [
-    {
-      id: 1,
-      title: "title",
-      discountRate: "30",
-      originalPrice: "10000",
-      discountPrice: "7000",
-    },
-    {
-      id: 2,
-      title: "title",
-      discountRate: "45",
-      originalPrice: "10000",
-      discountPrice: "5500",
-    },
-    {
-      id: 3,
-      title: "title",
-      discountRate: "10",
-      originalPrice: "10000",
-      discountPrice: "9000",
-    },
-    {
-      id: 4,
-      title: "title",
-      discountRate: "10",
-      originalPrice: "10000",
-      discountPrice: "9000",
-    },
-    {
-      id: 5,
-      title: "title",
-      discountRate: "10",
-      originalPrice: "10000",
-      discountPrice: "9000",
-    },
-  ];
+async function fetchProductList() {
+  const response = await fetch("https://fakestoreapi.com/products");
+  const productList = await response.json();
+  return productList;
+}
+
+export default async function Home() {
+  const productList = await fetchProductList();
 
   return (
     <div>
       <main className="grid grid-cols-4 gap-4">
-        {mockItemList.map((item) => (
+        {productList.map((item) => (
           <div
             key={item.id}
-            className="flex flex-col items-center justify-center border-2 border-gray-300 rounded-md p-4"
+            className="flex flex-col gap-2 items-center justify-center border-2 border-gray-300 rounded-md p-4"
           >
-            <Image
-              className="border-2 border-gray-300 rounded-md"
-              src="/sample.png"
-              alt="sample image"
-              width={100}
-              height={100}
-            />
-            <div className="flex gap-2 items-center justify-center">
-              <span className="text-lg font-bold">{item.title}</span>
-              <span className="text-red-500">{item.discountRate}%</span>
-              <span className="text-gray-500 line-through">{item.originalPrice}</span>
-              <span className="text-blue-500">{item.discountPrice}</span>
-            </div>
+            <Image src={item.image} alt={item.title} width={100} height={100} />
+            <span className="text-lg font-bold">{item.title}</span>
+            <span className="text-gray-500 line-through">{item.price}</span>
           </div>
         ))}
       </main>
