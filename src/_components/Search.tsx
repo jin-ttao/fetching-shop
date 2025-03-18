314e0e46c7a6959d937f8336a6f1dda9cab445df
Jintae Song <w.jinttao@gmail.com>
2025-03-19 04:48:06 +0900

    Feat: add Search mockup

---
+++ src/_components/Search.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Search() {
+  return (
+    <div>
+      <input
+        type="text"
+        className="w-[20vw] h-10 border-2 border-gray-300 rounded px-2"
+      />
+    </div>
+  );
+}
