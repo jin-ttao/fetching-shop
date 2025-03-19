7bcb47d758dbb8ab643449ba3837adc2c1f50f43
Jintae Song <w.jinttao@gmail.com>
2025-03-19 13:49:16 +0900

    Refactor: change prop name

--- src/_components/ProductItem.tsx
+++ src/_components/ProductItem.tsx
@@ -4,23 +4,23 @@ import { ProductDetail } from "@/types";
 
 const IMAGE_SIZE = 500;
 
-export default function ProductItem({ item }: { item: ProductDetail }) {
-  const { discountRate, price, stock } = item;
+export default function ProductItem({ product }: { product: ProductDetail }) {
+  const { discountRate, price, stock } = product;
   const discountedPrice = Math.floor(price - (price * discountRate) / 100);
-  const imageUrl = item.images[0];
+  const imageUrl = product.images[0];
   const isValidImage = imageUrl.startsWith("https://");
   const isSoldOut = stock === 0;
 
   return (
     <div
-      key={item.id}
+      key={product.id}
       className={`flex align-start flex-col gap-2 items-center justify-start p-4 ${isSoldOut ? "opacity-30" : "cursor-pointer hover:border-2 hover:border-orange-500 hover:shadow-md hover:rounded"}`}
     >
       <div className="flex justify-center w-full">
         {isValidImage ? (
           <Image
             src={imageUrl}
-            alt={item.title}
+            alt={product.title}
             width={IMAGE_SIZE}
             height={IMAGE_SIZE}
             className="w-full h-full max-w-xs max-h-xs rounded"
@@ -30,10 +30,10 @@ export default function ProductItem({ item }: { item: ProductDetail }) {
           <div className="w-full h-full max-w-xs max-h-xs rounded bg-gray-200" />
         )}
       </div>
-      <span className="text-lg font-semibold">{item.title}</span>
+      <span className="text-lg font-semibold">{product.title}</span>
       <div className="flex gap-2">
-        <span className="text-xl font-semibold text-orange-500">{item.discountRate}%</span>
-        <span className="text-xl text-gray-500 line-through">${item.price}</span>
+        <span className="text-xl font-semibold text-orange-500">{product.discountRate}%</span>
+        <span className="text-xl text-gray-500 line-through">${product.price}</span>
         <span className="text-2xl font-semibold text-black">${discountedPrice}</span>
       </div>
       {isSoldOut && <span className="text-base text-gray-500">Sold Out</span>}
